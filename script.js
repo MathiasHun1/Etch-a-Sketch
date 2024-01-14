3f30196a3e922716b3291705405ad2a93c5d2cb2
MathiasHun <mathias.hun@gmail.com>
2024-01-14 14:27:15 +0100

    add a random color draw feature with a button

--- script.js
+++ script.js
@@ -9,13 +9,42 @@ const resetButton = document.createElement('button');
 resetButton.setAttribute('id', 'reset-button');
 resetButton.textContent = "reset";
 body.appendChild(resetButton);
+resetButton.style.backgroundColor = 'aquamarine';
+
+// add color button
+const colorButton = document.createElement('button');
+colorButton.setAttribute('id', 'color-button');
+colorButton.innerText = 'Rainbow';
+body.appendChild(colorButton);
+colorButton.style.backgroundColor = 'aquamarine';
+
+// add color-button flag
+let colorFlag = false;
+colorButton.addEventListener('click', () => {
+    if (colorFlag === false) {
+        colorFlag = true;
+    } else if (colorFlag === true) {
+        colorFlag = false;
+    }
+
+    if (colorButton.style.backgroundColor === 'aquamarine') {
+        colorButton.style.backgroundColor = 'darkcyan';
+    } else {
+        colorButton.style.backgroundColor = 'aquamarine';
+    }
+});  
 
 //resetting --OK
 resetButton.addEventListener('click', () => {
     container.innerHTML = '';
     let rows = getUserInput();
     createCanvas(container, rows);
+    colorFlag = false;
+    colorButton.style.backgroundColor = 'aquamarine';
 });
+
+resetButton.addEventListener('mousedown', () => resetButton.style.backgroundColor = 'darkcyan');
+resetButton.addEventListener('mouseup', () => resetButton.style.backgroundColor = 'aquamarine');
 
 // mouse-down flag --OK
 let mouseDown = false;
@@ -26,15 +55,23 @@ container.addEventListener('mouseup', () => {
     mouseDown = false;
 });
 // implementation of the drawing, using event object --OK
-container.addEventListener('mouseover', (event) => {
+container.addEventListener('mousemove', (event) => {
     if (mouseDown === true) {
+        let red = randomRGB();
+        let green = randomRGB();
+        let blue = randomRGB();
         hoveredDiv = event.target;
-        hoveredDiv.style.backgroundColor = 'black';
+        if (colorFlag === false) {
+            hoveredDiv.style.backgroundColor = 'black';
+        } else if (colorFlag === true) {
+            hoveredDiv.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        }
+
         if (hoveredDiv === container) {
             container.style.backgroundColor = 'white';
         }
     }
-})
+});
 
 // create canvas func --OK
 function createCanvas (cont, rows) {
@@ -57,6 +94,9 @@ function getUserInput() {
     return Math.abs(input);
 }
 
+function randomRGB () {
+    return Math.floor(Math.random() * 255);
+}
 
 
 
